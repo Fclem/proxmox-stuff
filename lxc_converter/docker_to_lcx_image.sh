f4426ca37db5c68e295a3774c63d0539036b1b82
Clément Fiere <clement.fiere@eficode.com>
2023-03-17 15:03:40 +0200

    fix paths2

--- lxc_converter/docker_to_lcx_image.sh
+++ lxc_converter/docker_to_lcx_image.sh
@@ -2,16 +2,19 @@
 
 #lxc-create <<name>> -t oci -- --url docker://alpine:latest
 
-apt update && apt install skopeo umoci jq -y
+# inputs
 target_name="npm" # TODO read
-target_rootfs="${lxcpath}/${target_name}/rootfs"
 lxcpath="/root/lxc" # TODO read
-target_merged="${lxcpath}/${target_name}-merged"
-target_merged_rootfs="${target_merged}/rootfs"
-target_merged_base_image="${target_merged}/${base_image}"
 target_img="jc21/nginx-proxy-manager:latest" # TODO read
 base_image="debian-10-standard_10.7-1_amd64.tar.gz" # TODO read
+# variables
+target_path="${lxcpath}/${target_name}"
+target_rootfs="${target_path}/rootfs"
+target_merged="${target_path}-merged"
+target_merged_rootfs="${target_merged}/rootfs"
+target_merged_base_image="${target_merged}/${base_image}"
 
+apt update && apt install skopeo umoci jq -y && \
 # make lxcpath folder
 mkdir -p ${lxcpath} | true
 # make merged folder
@@ -19,7 +22,7 @@ mkdir -p ${target_merged} | true
 # convert docker image to LCI
 lxc-create ${target_name} -t oci --lxcpath ${lxcpath} -- --url docker://${target_img} && \
 # copy base image
-cp /var/lib/vz/template/cache/${base_image} ${target_merged}/ && \ # TODO replace with rsync
+cp /var/lib/vz/template/cache/${base_image} ${target_merged}/ && \  # TODO replace with rsync
 tar xvf ${target_merged_base_image} ${target_merged_rootfs}/ && \
 rm ${target_merged_base_image}
 # copy container rootfs over base image
